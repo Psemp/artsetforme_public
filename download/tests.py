d3c4f63c5795b0421593ee9b34931286ac664c9e
Psemp <57976914+Psemp@users.noreply.github.com>
2021-10-09 00:40:35 +0200

    testing a new download testing feature hopefully not filling the tree with useless empty pdfs.

--- download/tests.py
+++ download/tests.py
@@ -1,4 +1,4 @@
-from django.test import TestCase
+from django.test import TestCase, override_settings
 from django.urls import reverse
 from django.core.files import File
 from django.utils import timezone
@@ -7,9 +7,12 @@ import mock
 
 from .models import Downloadable
 
+TEST_DIR = "test_data"
+
 
 class DownloadTest(TestCase):
 
+    @override_settings(MEDIA_ROOT=(TEST_DIR + '/media'))
     def setUp(self):
         self.file_mock = mock.MagicMock(spec=File)
         self.file_mock.name = 'test.pdf'
