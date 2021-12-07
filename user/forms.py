5d3d3c46457e9a255520a19a73d6e69d0d0ecbf8
Psemp <57976914+Psemp@users.noreply.github.com>
2021-12-07 14:35:26 +0100

    micro correction

--- user/forms.py
+++ user/forms.py
@@ -1,10 +1,9 @@
-from django import forms
-from django.contrib.auth.models import User
-from django.contrib.auth.forms import UserCreationForm
-from django.db import models
-from django.http import request
-from .models import Profile
 from backoffice.models import Topic
+from django import forms
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.models import User
+
+from .models import Profile
 
 
 class UserRegisterForm(UserCreationForm):
